ddd39c3406c9f23ea4a6ab6afcc43ca177595d10
Jesse Carter <jesse.carter@gmail.com>
2018-12-23 16:40:31 -0500

    helper script to randomly pick atom and pass to pka script

---
+++ run-pka.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# simple bash script to select atom and velocity and run a pka simulation
+
+# set some simple parameters
+export pka_energy=1000.0  # pka energy in eV
+export pka_mass=55.845  # pka mass in amu
+
+# LAMMPS dump file containing atoms to be selected to be a PKA
+export atom_file="dump/dump.shell-10.txt"
+
+# select atom from file and velocity vector that points at the simulation box origin
+data_array=(`python -c "import os; import lammps_helper as l; print ' '.join(map(str, l.pick_atom_and_velocity(os.environ['atom_file'], float(os.environ['pka_energy']), float(os.environ['pka_mass']))))"`)
+
+# break out info
+pka_id=${data_array[0]}
+vx=${data_array[1]}
+vy=${data_array[2]}
+vz=${data_array[3]}
+
+# print info
+echo "for PKA:"
+echo "  using atom id: $pka_id"
+echo "  x-velocity: $vx"
+echo "  y-velocity: $vy"
+echo "  z-velocity: $vz"
+
+# clean up before running
+rm -rf dump/dumpall.*
+
+# run pka simulation and use the above info
+ mpiexec -np 4 ./lammps -in in.pka -v pka_id $pka_id -v vx $vx -v vy $vy -v vz $vz
