5feb11b0e36b57c52382763d6cb1cf177a7f526e
Jesse Carter <jesse.carter@gmail.com>
2018-12-30 22:28:45 -0500

    output thermo history and add a function to read it

--- lammps_helper.py
+++ lammps_helper.py
@@ -2,32 +2,43 @@ import pandas as pd
 import numpy as np
 
 
-def read_dump(filename, read_header=False):
+def read_file(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
-        # loop through looking for keywords
-        for i, line in enumerate(lines):
-            linestrip = line.strip()
-            if linestrip.startswith('ITEM: TIMESTEP'):
-                timestep = int(lines[i+1].strip())
-            if linestrip.startswith('ITEM: NUMBER OF ATOMS'):
-                atom_number = int(lines[i+1].strip())
-            if linestrip.startswith('ITEM: BOX BOUNDS'):
-                xlo, xhi = [float(a) for a in lines[i+1].strip().split()]
-                ylo, yhi = [float(a) for a in lines[i+2].strip().split()]
-                zlo, zhi = [float(a) for a in lines[i+3].strip().split()]
-            if linestrip.startswith('ITEM: ATOMS'):
-                columns = linestrip.split()[2:]
-                break
+        return [line.strip() for line in lines]
+
+
+def read_dump(filename, return_header=False):
+    lines = read_file(filename)
+    # loop through looking for keywords
+    for i, line in enumerate(lines):
+        if line.startswith('ITEM: TIMESTEP'):
+            timestep = int(lines[i+1].strip())
+        if line.startswith('ITEM: NUMBER OF ATOMS'):
+            atom_number = int(lines[i+1].strip())
+        if line.startswith('ITEM: BOX BOUNDS'):
+            xlo, xhi = [float(a) for a in lines[i+1].strip().split()]
+            ylo, yhi = [float(a) for a in lines[i+2].strip().split()]
+            zlo, zhi = [float(a) for a in lines[i+3].strip().split()]
+        if line.startswith('ITEM: ATOMS'):
+            columns = line.split()[2:]
+            break
     info = {'timestep': timestep, 'number of atoms': atom_number,
             'xlo': xlo, 'xhi': xhi, 'ylo': ylo, 'yhi': yhi, 'zlo': zlo, 'zhi': zhi}
 
     df = pd.read_csv(filename, index_col=0, skiprows=9, names=columns, delim_whitespace=True)
 
-    if read_header:
+    if return_header:
         return df, info
 
     return df
+
+
+def read_ave_time(filename):
+    lines = read_file(filename)
+    # keywords are on the second line
+    columns = lines[1].split()[2:]  # these are the data headers
+    return pd.read_csv(filename, index_col=0, skiprows=2, names=columns, delim_whitespace=True)
 
 
 def unit_vector(v):
