27de0cfbdc5428a99694d2b9e7da24db559aedc9
Jesse Carter <jesse.carter@gmail.com>
2019-08-12 21:49:46 -0400

    store lammps dumps in different directories

--- run-pka.sh
+++ run-pka.sh
@@ -3,7 +3,7 @@
 # simple bash script to select atom and velocity and run a pka simulation
 
 # set some parameters, will attempt to look for restart file based on this information
-end_time=1  #  set the (minimum) end time of the simulation, units are picoseconds
+end_time=0.1  #  set the (minimum) end time of the simulation, units are picoseconds
 pka_energy=1  # pka energy in keV
 Nx=10  # (half) box size
 T=300  # temperature
@@ -34,14 +34,19 @@ rm -rf $dump_dir
 mkdir -p $dump_dir
 echo "writing output to $dump_dir..."
 
+# make some more output directories for different dumps
+mkdir $dump_dir/all $dump_dir/defects $dump_dir/combined_defects
+
 # run pka simulation and use the above info
 mpiexec -np 4 ./lammps -in in.pka -v pka_id $pka_id -v vx $vx -v vy $vy -v vz $vz -v end_time $end_time -v dump_dir $dump_dir -v Nx $Nx -v T $T
 
 # combine separate int/vac dumps into a single
-for file in $dump_dir/dump.ints.*.txt
+cd $dump_dir/defects
+for file in dump.ints.*.txt
 do
   echo "processing $file..."
   vac_file=`echo $file | sed 's/ints/vacs/'`
   out_file=`echo $file | sed 's/ints/defects/'`
-  python combine_defect_files.py $file $vac_file $out_file
+  python ../../../../combine_defect_files.py $file $vac_file ../combined_defects/$out_file
 done
+cd ../..
