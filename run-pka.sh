1957e379a01e93a1d4514e7577488141fc2211e4
Jesse Carter <jesse.carter@gmail.com>
2019-08-29 21:31:23 -0400

    work out of each run's unique directory

--- run-pka.sh
+++ run-pka.sh
@@ -3,7 +3,7 @@
 # simple bash script to select atom and velocity and run a pka simulation
 
 # set some parameters, will attempt to look for restart file based on this information
-end_time=0.1  #  set the (minimum) end time of the simulation, units are picoseconds
+end_time=1  #  set the (minimum) end time of the simulation, units are picoseconds
 pka_energy=1  # pka energy in keV
 Nx=10  # (half) box size
 T=300  # temperature
@@ -30,23 +30,29 @@ echo "  z-velocity: $vz"
 # make a unique directory for this run
 run_number=`python get_run_number.py $pka_energy $T $Nx`
 dump_dir=dump/$pka_energy-keV-T-$T-Nx-$Nx/run-$run_number
-rm -rf $dump_dir
+rm -rf $dump_diry
 mkdir -p $dump_dir
 echo "writing output to $dump_dir..."
 
+# work out of dump directory
+pka_dir=$PWD
+cd $dump_dir
+
 # make some more output directories for different dumps
-mkdir $dump_dir/all $dump_dir/defects $dump_dir/combined_defects
+mkdir all defects combined_defects clusters
 
 # run pka simulation and use the above info
-mpiexec -np 4 ./lammps -in in.pka -v pka_id $pka_id -v vx $vx -v vy $vy -v vz $vz -v end_time $end_time -v dump_dir $dump_dir -v Nx $Nx -v T $T
+mpiexec -np 4 $pka_dir/lammps -in $pka_dir/in.pka \
+  -v pka_id $pka_id -v vx $vx -v vy $vy -v vz $vz -v end_time $end_time -v Nx $Nx -v T $T \
+  -v restart_dir $pka_dir/restart -v potential_dir $pka_dir/potentials
 
 # combine separate int/vac dumps into a single
-cd $dump_dir/defects
+cd defects
 for file in dump.ints.*.txt
 do
   echo "processing $file..."
   vac_file=`echo $file | sed 's/ints/vacs/'`
   out_file=`echo $file | sed 's/ints/defects/'`
-  python ../../../../combine_defect_files.py $file $vac_file ../combined_defects/$out_file
+  python $pka_dir/combine_defect_files.py $file $vac_file ../combined_defects/$out_file
 done
-cd ../..
+
